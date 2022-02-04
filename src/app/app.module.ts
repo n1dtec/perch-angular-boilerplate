88f08c59ca394f527fe1d06d88432ef2163eb7fd
Hector Peña Torres <htorres@myperch.io>
2022-02-04 15:01:39 -0600

    innitial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateModule } from '@ngx-translate/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
+import { ShellModule } from './shell/shell.module';
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule,
+    TranslateModule.forRoot(),
+    NgbModule,
+    ShellModule,
+    HomeModule,
+    AppRoutingModule, // must be imported as the last module as it contains the fallback route
+  ],
+  declarations: [AppComponent],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
